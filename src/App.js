18602f412cf7f93e426a38a1b510eb02c585f94c
Azeem <108066377+Azeem-0@users.noreply.github.com>
2023-09-29 21:04:33 +0530

    Added client side routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import Home from "./Home";
+import Home from "./components/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Footer, Header } from "./components/Partials";
+import { Dashboard } from "./components/Dashboard";
+import { About } from "./components/About";
 function App() {
   return (
     <div>
-      <Home/>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact Component={Home} />
+          <Route path="/about" exact Component={About} />
+          <Route path="/dashboard" exact Component={Dashboard} />
+          <Route path="/auth" exact Component={Home} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Partials.js
@@ -0,0 +1,20 @@
+
+function Header() {
+    return (
+        <div id="header">
+            <a href="/" className="nav-link">Home</a>
+            <a href="/dashboard" className="nav-link">DashBoard</a>
+            <a href="/about" className="nav-link">About</a>
+            <a href="/auth" className="nav-link">SignIn</a>
+        </div>
+    )
+}
+function Footer() {
+    return (
+        <div id="footer">
+            All rights reserved 2022-2023
+        </div>
+    )
+}
+export { Header };
+export { Footer };
