012a72e0aeb2e5ace9e969d64dc5faa68565dd25
Azeem <108066377+Azeem-0@users.noreply.github.com>
2023-09-29 21:22:21 +0530

    Removed About page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Home from "./components/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Footer, Header } from "./components/Partials";
 import { Dashboard } from "./components/Dashboard";
-import { About } from "./components/About";
 function App() {
   return (
     <div>
@@ -10,7 +9,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" exact Component={Home} />
-          <Route path="/about" exact Component={About} />
           <Route path="/dashboard" exact Component={Dashboard} />
           <Route path="/auth" exact Component={Home} />
         </Routes>
